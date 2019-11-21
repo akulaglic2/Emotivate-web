553ed276ceb026cc42322dfee7e7d09b2b5276dc
Dzemo <dzemal.ibric@klika.ba>
2019-11-21 15:01:39 +0100

    Fix authors

--- AuthorAjax.js
+++ AuthorAjax.js
@@ -8,7 +8,6 @@ var AuthorAjax = (function () {
                     var string = "";
                     if (ajax.readyState == 4 && ajax.status == 200) {
                         var jsonResponse= JSON.parse(ajax.response)
-                        if (!jsonResponse.authors) return;
                         string += "<select id='spinnerAuthors' >";
                         string += "<option value='NONE'>NONE</option>"
 
